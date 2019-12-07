238234df27533837e19a47ae4d77915311ed3885
shiva-sk <shivaysks@gmail.com>
2019-12-07 19:42:58 +0530

    hello

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,32 @@
+package org.add;
+
+public class GreensTech {
+	private void greensOmr() {
+		// TODO Auto-generated method stub
+System.out.println("address is OMR");
+	}
+private void greensAdayar() {
+	// TODO Auto-generated method stub
+System.out.println("address is adayar ");
+}
+private void greenstambaram() {
+	// TODO Auto-generated method stub
+System.out.println("address is tambaram");
+}
+private void greensVelachery() {
+	// TODO Auto-generated method stub
+System.out.println("address is velachery");
+}
+private void greensAnnaNag() {
+	// TODO Auto-generated method stub
+System.out.println("address is annanagar");
+}
+public static void main(String[] args) {
+	GreensTech t= new GreensTech();
+	t.greensAdayar();
+	t.greensAnnaNag();
+	t.greensOmr();t.greenstambaram();t.greensVelachery();
+}
+
+
+}
